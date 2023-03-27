834997a3ccac35a5f79733657fe7f4ede7e2cc47
uSELESScASE <uselesscaselaptop@gmail.com>
2023-03-27 15:23:39 +0300

    AAA

--- src/main/java/frc/robot/subsystems/PAShuffle.java
+++ src/main/java/frc/robot/subsystems/PAShuffle.java
@@ -1,26 +1,89 @@
 package frc.robot.subsystems;
 
+import java.util.Map;
+
+import org.opencv.core.Mat;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import frc.robot.Constants;
 import frc.robot.Robot;
 
 public class PAShuffle {
     private static Gamepad xGamepad;
+    private static Thread m_visionThread;
     
     public static void onStart() {
-        xGamepad = Gamepad.getInstance(Constants.XboxPort);
+        xGamepad = Gamepad.getInstance(Constants.JoystickPort);
 
-        Shuffleboard.getTab("uSELESScASE Shuffleboard");
-        Shuffleboard.startRecording();
+        Shuffleboard.getTab("uSELESScASE General");
+        Shuffleboard.selectTab("uSELESScASE General");
 
-        SmartDashboard.setDefaultNumber("Get R-Y Axis", 0.0);
-        SmartDashboard.setDefaultNumber("Get R-X Axis", 0.0);
-        SmartDashboard.setDefaultNumber("Get RTA", 0.0);
-        SmartDashboard.setDefaultNumber("Get Arm Angle", 0.0);
+        Shuffleboard.getTab("uSELESScASE General")
+        .add("Left X Axis", 0.0)
+        .withWidget(BuiltInWidgets.kNumberBar)
+        .withProperties(Map.of("min",-1 , "max",1));
+        Shuffleboard.getTab("uSELESScASE General")
+        .add("Left Y Axis", 0.0)
+        .withWidget(BuiltInWidgets.kNumberBar)
+        .withProperties(Map.of("min",-1 , "max",1));
+        Shuffleboard.getTab("uSELESScASE General")
+        .add("Right Trigger Axis", 0.0)
+        .withWidget(BuiltInWidgets.kNumberSlider)
+        .withProperties(Map.of("min",0 , "max",1));
+        Shuffleboard.getTab("uSELESScASE General")
+        .add("Right Y Axis", 0.0)
+        .withWidget(BuiltInWidgets.kNumberBar)
+        .withProperties(Map.of("min",-1 , "max",1));
+
         SmartDashboard.setDefaultBoolean("Set Off", false);
         SmartDashboard.setDefaultBoolean("Set Forward", false);
-        SmartDashboard.setDefaultBoolean("Set Reverse", false);   
+        SmartDashboard.setDefaultBoolean("Set Reverse", false);
+
+        m_visionThread =
+        new Thread(
+            () -> {
+              int width = 400;
+              int height = 240;
+
+              // Get the UsbCamera from CameraServer
+              UsbCamera camera = CameraServer.startAutomaticCapture();
+              // Set the resolution
+              camera.setResolution(width, height);
+
+              // Get a CvSink. This will capture Mats from the camera
+              CvSink cvSink = CameraServer.getVideo();
+              // Setup a CvSource. This will send images back to the Dashboard
+              CvSource outputStream = CameraServer.putVideo("Robot Kamerasi", width, height);
+
+              // Mats are very memory expensive. Lets reuse this Mat.
+              Mat mat = new Mat();
+
+              // This cannot be 'true'. The program will never exit if it is. This
+              // lets the robot stop this thread when restarting robot code or
+              // deploying.
+              while (!Thread.interrupted()) {
+                // Tell the CvSink to grab a frame from the camera and put it
+                // in the source mat.  If there is an error notify the output.
+                if (cvSink.grabFrame(mat) == 0) {
+                  // Send the output the error.
+                  outputStream.notifyError(cvSink.getError());
+                  // skip the rest of the current iteration
+                  continue;
+                }
+                // Give the output stream a new image to display
+                outputStream.putFrame(mat);
+              }
+            });
+
+    m_visionThread.setDaemon(true);
+    m_visionThread.start();
     }
 
     public static void getRobotStatus() {
@@ -36,9 +99,8 @@ public class PAShuffle {
         double rot = xGamepad.getSteer();
         double deg = xGamepad.getRightThumbY();
 
-        SmartDashboard.putNumber("Get R-Y Axis", spd);
-        SmartDashboard.putNumber("Get R-X Axis", rot);
-        SmartDashboard.putNumber("Get Arm Angle", deg);
-        SmartDashboard.putNumber("Get RTA", Constants.throttleMult);
+        SmartDashboard.putNumber("Left X Axis", spd);
+        SmartDashboard.putNumber("Left Y Axis", rot);
+        SmartDashboard.putNumber("Right Trigger Axis", spd);
     }
 }
