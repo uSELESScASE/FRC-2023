704987061c644074db62203dc5ea4f73cb2c551f
cockrochi <mrardaacar@hotmail.com>
2023-03-28 10:58:42 +0300

    aaa

--- src/main/java/frc/robot/subsystems/Gyroscope.java
+++ src/main/java/frc/robot/subsystems/Gyroscope.java
@@ -12,33 +12,32 @@ public class Gyroscope {
 
     private static byte L3GD20H_ADDRESS;
     private static int REGISTER_PWR_MGMT;
-    private static int REGISTER_GYRO;
+    private static int LOW_ODR;
+    private static int CTRL_REG_1;
+    private static int CTRL_REG_4;
 
 
     Gyroscope(){
 
         L3GD20H_ADDRESS = 0x6B;
         REGISTER_PWR_MGMT = 0x20;
-        REGISTER_GYRO = 0x29;
     }
 
-    private I2C accelerometer = new I2C(I2C.Port.kMXP ,L3GD20H_ADDRESS);
+    private I2C accelerometer = new I2C(I2C.Port.kOnboard ,L3GD20H_ADDRESS);
     private byte[] buffer = new byte[6];
 
 
     public void gyroRead(){
-
-        accelerometer.read(REGISTER_GYRO, 6, buffer);
-        
-        int x = (buffer[0] << 8) | buffer[1];
-        int y = (buffer[2] << 8) | buffer[3];
-        int z = (buffer[4] << 8) | buffer[5];
-    
-        //System.out.println(x + "," + y + "," + z);
         
     }
 
     public void wake_gyro(){
         accelerometer.write(REGISTER_PWR_MGMT, 0x0F);
+        accelerometer.write(CTRL_REG_1, 0x6F);
+        accelerometer.write(CTRL_REG_4, 0x00);
+        accelerometer.write(LOW_ODR, 0x00);
     }
+
+
+
 }
