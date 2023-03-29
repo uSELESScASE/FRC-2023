fed126671cc3cef44c9311748246ff4f2d92ea8a
cockrochi <mrardaacar@hotmail.com>
2023-03-29 09:35:24 +0300

    aaa

--- src/main/java/frc/robot/subsystems/Gyroscope.java
+++ src/main/java/frc/robot/subsystems/Gyroscope.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.hal.util.UncleanStatusException;
 import edu.wpi.first.wpilibj.SerialPort;
 
@@ -18,16 +20,24 @@ public class Gyroscope {
             arduino = new SerialPort(9600, SerialPort.Port.kUSB);
             System.out.println("check!");
             arduino.setTimeout(0.001);
-        }
-        catch (UncleanStatusException e){
-        System.out.println("ERROR: Unable to connect to Arduino");
+         }
+         catch (UncleanStatusException e){
+         System.out.println("ERROR: Unable to connect to Arduino");
        }
 
     }
 
     public void readGyro(){
-        arduino.readString();
-        System.out.println(arduino.readString());
+        ArrayList<byte[]> arduino_out = new ArrayList<byte[]>(6);
+  
+        arduino_out.add(arduino.read(6));
+        System.out.println("raw arduino data" + arduino_out);
+
+        String x_val = arduino_out.get(0).toString().concat(arduino_out.get(1).toString());
+        String y_val = arduino_out.get(2).toString().concat(arduino_out.get(3).toString());
+        String z_val = arduino_out.get(4).toString().concat(arduino_out.get(5).toString());
+
+        System.out.println("X"+ x_val + "Y" + y_val + "Z" + z_val);
     }
   
 
